6c2c4071313dfeb0848d9db391b91ddf401c8b16
winterrosy <wish759@naver.com>
2022-01-18 21:58:47 +0900

    Test2

--- src/test.java
+++ src/test.java
@@ -3,6 +3,11 @@ public class test {
 
 	public static void main(String[] args) {
 		System.out.println("테스트");
+		System.out.println("테스트");
+		System.out.println("테스트");
+		System.out.println("테스트");
+		System.out.println("테스트");
+		System.out.println("테스트");
 	}
 
 }
